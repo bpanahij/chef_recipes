24460913abb94b5c8588cb80e4924662cb1f3834
Brian Johnson <brian@pjohnson.info>
2014-08-03 15:40:30 -0700

    Initial Commit of the recipes I like

---
+++ deploy-config/metadata.rb
@@ -0,0 +1,9 @@
+name             'deploy-config'
+maintainer       'brian@pjohnson.info'
+maintainer_email 'brian@pjohnson.info'
+license          'All rights reserved'
+description      'Configures Node App with node_modules/config/index.json'
+long_description 'Deploy configuration'
+version          '1.0.0'
+
+recipe "deploy_config::default", "Configures node app with config module"
---
+++ deploy-config/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: passport_config
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+node[:deploy].each do |app, deploy|
+  directory "#{deploy[:deploy_to]}/node_modules/config" do
+      owner "deploy"
+      group "www-data"
+      mode 0774
+      recursive true
+      action :create
+    end
+  file File.join(deploy[:deploy_to], 'node_modules', 'config', 'index.json') do
+    content JSON.dump(node[:passport][app])
+  end
+end
